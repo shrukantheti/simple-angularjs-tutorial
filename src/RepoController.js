022bc4c097045d84f02947f9f6fdb5a3fd73f45c
Shruthi K <shrukantheti@users.noreply.github.com>
2015-05-06 10:36:35 -0400

    Create RepoController.js

---
+++ src/RepoController.js
@@ -0,0 +1,25 @@
+
+(function(){
+    
+    var module = angular.module("sampleApp");
+    
+    var RepoCtrl = function($scope, $routeParams, githubService){
+        var reponame = $routeParams.reponame;
+        var username = $routeParams.username;
+        
+        var onRepo = function(data){
+            $scope.repo = data;
+            //console.log(data);
+        };
+        
+        var onError = function(reason){
+            $scope.error = reason;
+        };
+        
+        githubService.getRepoDetails(username, reponame)
+            .then(onRepo, onError);
+    };
+    
+    module.controller('RepoCtrl',["$scope", "$routeParams", "githubService", RepoCtrl]);
+    
+}());
