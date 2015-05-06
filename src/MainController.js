eda6c6cd8f2448fffcf05137c2677af2375d58d5
Shruthi K <shrukantheti@users.noreply.github.com>
2015-05-06 10:36:11 -0400

    Create MainController.js

---
+++ src/MainController.js
@@ -0,0 +1,32 @@
+// Code goes here
+(function() {
+    var app = angular.module("sampleApp");
+    var MainCtrl = function($scope, $interval, $location) {
+        
+        var decrementCountdown = function() {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        };
+
+        var countdownInterval = null;
+        var startCountdown = function() {
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        $scope.search = function(username) {
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
+            $location.path("/user/" + username);
+        };
+
+        $scope.username = "Angular";
+        $scope.countdown = 5;
+        startCountdown();
+    };
+    app.controller('MainCtrl', ["$scope", "$interval", "$location", MainCtrl]);
+
+})();
